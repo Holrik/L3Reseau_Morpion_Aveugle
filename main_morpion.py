0a4cdf094d8ba4862bcf79ac0bc7627bf06e5f85
Holrik <christophe.caubet@etu.u-bordeaux.fr>
2017-01-06 18:56:52 +0100

    Broke the infinite VS-IA.

--- main_morpion.py
+++ main_morpion.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 from grid import *
-#from client import *
-#from main import *
+import  random
 from socket import error as SocketError
 import errno
 import socket
@@ -207,6 +206,34 @@ def main_server():
 
 
 
+def against_IA():
+    grids = [grid(), grid(), grid()]
+    current_player = J1
+    grids[J1].display()
+    while grids[0].gameOver() == -1:
+        if current_player == J1:
+            shot = -1
+            while shot <0 or shot >=NB_CELLS:
+                shot = int(input ("quel case allez-vous jouer ?"))
+        else:
+            shot = random.randint(0,8)
+            while grids[current_player].cells[shot] != EMPTY:
+                shot = random.randint(0,8)
+        if (grids[0].cells[shot] != EMPTY):
+            grids[current_player].cells[shot] = grids[0].cells[shot]
+        else:
+            grids[current_player].cells[shot] = current_player
+            grids[0].play(current_player, shot)
+            current_player = current_player%2+1
+        if current_player == J1:
+            grids[J1].display()
+    print("game over")
+    grids[0].display()
+    if grids[0].gameOver() == J1:
+        print("You win !")
+    else:
+        print("you loose !")
+
 # Gets a connection with the server if possible. If not possible, exit
 def init_client(point):
 	continuer = ""
@@ -223,7 +250,19 @@ def init_client(point):
 		else:
 			if continuer == "n":
 				print("Joueur contre l'IA")
-				main()
+				against_IA()
+				try:
+					continuer = input ("Voulez vous rejouer une partie ? y/n :")
+				except ( EOFError):
+				# If the value is anything but an int
+					print("Mauvaise valeur ! Entrez y ou n !")
+					time.sleep(.2)
+				if continuer != "n" and continuer != "y":
+					print("Mauvaise valeur ! Entrez y ou n !")
+					time.sleep(.2)
+				else:
+					if continuer == "n":
+						exit()
 				init_client(point)
 
 	print("Joueur contre Joueur")
